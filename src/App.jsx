215727083e7a10e8470d380795391a2de026b230
Furkanfikret <dedefiko1111@gmail.com>
2025-07-13 21:24:54 +0300

    yayin frontend ilk commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+
+import { useState } from 'react'
+import './App.css'
+import UserTable from './compenents/UserTable';
+import axios from 'axios';
+import { useEffect } from 'react';
+
+function App() {
+  const [users,setUsers] = useState([]);
+  const [showTable,setShowtable] =useState(false);
+  const [digit,setDigit] = useState(0);
+  const increament = () =>{setDigit(prev => prev+1)}
+  const decreament = () =>{setDigit(prev => prev-1)}
+  const reset = () =>{setDigit(0)}
+  const api_url = import.meta.env.VITE_API_URL; 
+
+  useEffect(()=>{
+    axios.get(api_url)
+      .then(res => setUsers(res.data))
+      .catch(err => console.log(`Veri tabanına Bağlanılamadı ! - ${err}`))
+  },[])
+
+  return (
+   <div>
+    <h1>Frontend Home Page !</h1>
+    <h2>Value = {digit}</h2>
+    <button onClick={increament}>Increase</button>
+    <button onClick={decreament}>Decrease</button>
+    <button onClick={reset}>Reset</button>
+    <button onClick={()=>setShowtable(!showTable)}>
+      {
+        showTable ? 'Gizle' : 'Kullanıcıları Göster'
+      }
+    </button>
+    {showTable && <UserTable users={users} onClose={() => setShowtable(false)} />}
+   </div>
+  )
+}
+
+export default App
---
+++ src/compenents/UserTable.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import '../css/userTable.css'
+function UserTable({users,onClose}) {
+  return (
+    <div className='table-wrapper'>
+        <div className='table-header'>
+            <h2>Kullanıcı Tablosu</h2>
+            <button onClick={onClose} className='close-btn'>Kapat</button>
+        </div>
+        <table>
+            <thead>
+                <tr>
+                    <th>AD</th>
+                    <th>SOYAD</th>
+                    <th>CİNSİYET</th>
+                    <th>KAYIT TARİHİ</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    users.map( (user) => (
+                        <tr key={user.id}>
+                           <td>{user.name}</td>
+                           <td>{user.surname}</td>
+                           <td>{user.gender}</td>
+                           <td>{new Date(user.record_date).toLocaleDateString()}</td>     
+                        </tr>
+                    ))
+                }
+            </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default UserTable;
